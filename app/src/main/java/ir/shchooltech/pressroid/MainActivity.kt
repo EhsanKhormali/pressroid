86afd342c2c0cebf2171bdc136d074caf2bef71e
Ehsan <ehsankhormali@gmail.com>
2022-07-16 18:12:52 +0430

    chose color palette and some initial implementation for search

--- app/src/main/java/ir/shchooltech/pressroid/MainActivity.kt
+++ app/src/main/java/ir/shchooltech/pressroid/MainActivity.kt
@@ -1,11 +1,25 @@
 package ir.shchooltech.pressroid
 
+import android.app.SearchManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.SearchView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main_top,menu)
+        //associate searchable configuration with the searchView
+        val searchManager=getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        (menu.findItem(R.id.app_bar_search).actionView as SearchView).apply {
+            setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        }
+        return true
     }
 }
