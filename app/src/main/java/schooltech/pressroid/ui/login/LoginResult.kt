90cbeaa2ffbcd8b6586f734b6db331384ec6a791
Ehsan <ehsankhormali@gmail.com>
2022-08-10 01:14:12 +0430

    add login activity

---
+++ app/src/main/java/schooltech/pressroid/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package schooltech.pressroid.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
